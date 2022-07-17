1b3e427870c82c3602d6298cc9379e4b4f393ab1
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-17 16:15:04 +0530

    Update Bubble Sort.cpp

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -10,14 +10,21 @@ class Solution
     }
     void bubbleSort(int arr[], int n)
     {
+        bool flag=true;
         for(int i=0;i<n-1;i++)
         {
+            flag=true;
             for(int j=0;j<(n-i-1);j++)
             {
                 if(arr[j]>arr[j+1])
                 {
+                    flag=false;
                     swap(arr[j],arr[j+1]);
                 }
+            }
+            if(flag)
+            {
+                break;
             }
         }  
     }
