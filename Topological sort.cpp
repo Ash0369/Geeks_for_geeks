3a18439dc3b74dd24026554d3030892b4830c947
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-23 06:08:34 +0530

    Update Topological sort.cpp

--- Topological sort.cpp
+++ Topological sort.cpp
@@ -35,3 +35,50 @@ class Solution
         return result;
 	}
 };
+
+//Method-2 : BFS(Khan's Algorthim)
+
+class Solution
+{
+	public:
+        void bfs(int node,vector<int>&indegree,vector<int>&result,queue<int>&q,vector<int>adj[])
+        {
+            result.push_back(node);
+            for(auto x:adj[node])
+            {
+                indegree[x]--;
+                if(indegree[x]==0)
+                {
+                    q.push(x);
+                }
+            }
+        }
+	vector<int> topoSort(int v, vector<int> adj[]) 
+	{
+	    vector<int>visited(v,0);
+        vector<int>result;
+        queue<int>q;
+        vector<int>indegree(v,0);
+        for(int i=0;i<v;i++)
+        {
+            for(auto x:adj[i])
+            {
+                indegree[x]++;
+            }
+        }
+        for(int i=0;i<v;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            bfs(node,indegree,result,q,adj);
+        }
+        return result;
+	}
+};
