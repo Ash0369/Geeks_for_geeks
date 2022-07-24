a1bd22eb7202cba92855cb2329a4d71c42d90788
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-24 14:56:05 +0530

    Update Topological sort.cpp

--- Topological sort.cpp
+++ Topological sort.cpp
@@ -15,26 +15,26 @@ class Solution
         }
         st.push(node);
     }
-	vector<int> topoSort(int v, vector<int> adj[]) 
-	{
-	    vector<int>visited(v,0);
-        vector<int>result;
-        stack<int>st;
-        for(int i=0;i<v;i++)
-        {
-            if(!visited[i])
-            {
-                dfs(i,st,visited,adj);
-            }
-        }
-        while(!st.empty())
-        {
-            result.push_back(st.top());
-            st.pop();
-        }
-        return result;
-	}
-};
+    vector<int> topoSort(int v, vector<int> adj[]) 
+    {
+           vector<int>visited(v,0);
+           vector<int>result;
+           stack<int>st;
+           for(int i=0;i<v;i++)
+           {
+               if(!visited[i])
+               {
+                   dfs(i,st,visited,adj);
+               }
+           }
+           while(!st.empty())
+           {
+               result.push_back(st.top());
+               st.pop();
+           }
+           return result;
+    }
+}    
 
 //Method-2 : BFS(Khan's Algorthim)
 
