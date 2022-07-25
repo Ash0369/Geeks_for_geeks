837bb206763599a51aec290edefa8918f062895a
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-25 12:23:04 +0530

    Update Minimum Spanning Tree.cpp

--- Minimum Spanning Tree.cpp
+++ Minimum Spanning Tree.cpp
@@ -57,3 +57,61 @@ class Solution
         return sum;
     }
 };
+
+//Method-2 : Using Priority Queue (Test cases are faulty according to striver)
+
+class Solution
+{
+	public:
+    int spanningTree(int v, vector<vector<int>> adj[])
+    {
+        vector<int>distance(v,INT_MAX);
+        vector<int>parent(v,-1);
+        vector<bool>mst(v,false);
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+
+        distance[0]=0;
+        parent[0]=-1;
+
+        pq.push(make_pair(0,0));
+
+        for(int i=0;i<v-1;i++) 
+        {
+            //Find element with minimum key value
+
+            int node=pq.top().second;
+            pq.pop();
+            mst[node]=true;
+
+            //we have node as our minimum key value
+
+            for(auto x:adj[node])
+            {
+                if(mst[x[0]]==true)
+                {
+                    continue;
+                }
+                if(x[0]==parent[node])
+                {
+                    continue;
+                }
+
+                if(x[1]>distance[x[0]])
+                {
+                    continue;
+                }
+
+                parent[x[0]]=node;
+                distance[x[0]]=x[1];
+                pq.push(make_pair(distance[x[0]],x[0]));
+            }
+        }
+        int sum=0;
+        for(auto x:distance)
+        {
+            sum=sum+x;
+        }
+        return sum;
+    }
+};
