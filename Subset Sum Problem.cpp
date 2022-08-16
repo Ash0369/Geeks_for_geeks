58d12d80b1fff0e2e05add2a2c50460e6aaf6e58
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-17 04:56:12 +0530

    Update Subset Sum Problem.cpp

--- Subset Sum Problem.cpp
+++ Subset Sum Problem.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 bool check(vector<int>&arr,int sum,int index,vector<vector<int>>&dp)
 {
     if(sum==0)
@@ -29,5 +31,76 @@ public:
         int n=arr.size();
         vector<vector<int>>dp(n,vector<int>(sum+1,-1));
         return check(arr,sum,n-1,dp);
+    }
+};
+
+
+//Method-2 :
+
+class Solution
+{   
+public:
+    bool isSubsetSum(vector<int>arr, int sum)
+    {
+        int n=arr.size();
+        vector<vector<bool>>dp(n,vector<bool>(sum+1,false));
+        for(int i=0;i<n;i++)
+        {
+            dp[i][0]=true;
+        }
+        if(arr[0]<=sum)
+        {
+            dp[0][arr[0]]=true;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=sum;j++)
+            {
+                bool pick=false;
+                if(j>=arr[i])
+                {
+                    pick=dp[i-1][j-arr[i]];
+                }
+                bool not_pick=dp[i-1][j];
+                dp[i][j]=pick|not_pick;
+            }
+        }
+        return dp[n-1][sum];
+    }
+};
+
+//Method-3 : 
+
+class Solution
+{   
+public:
+    bool isSubsetSum(vector<int>arr, int sum)
+    {
+        int n=arr.size();
+        vector<bool>dp(sum+1,false);
+        vector<bool>curr(sum+1,false);
+        
+        dp[0]=true;
+        curr[0]=true;
+        
+        if(arr[0]<=sum)
+        {
+            dp[arr[0]]=true;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=sum;j++)
+            {
+                bool pick=false;
+                if(j>=arr[i])
+                {
+                    pick=dp[j-arr[i]];
+                }
+                bool not_pick=dp[j];
+                curr[j]=pick|not_pick;
+            }
+            dp=curr;
+        }
+        return dp[sum];
     }
 };
