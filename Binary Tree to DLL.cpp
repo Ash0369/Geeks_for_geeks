b5be5c99ae7e31b7c5f560edb4b75a8b9aa0a34f
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-28 05:15:23 +0530

    Update Binary Tree to DLL.cpp

--- Binary Tree to DLL.cpp
+++ Binary Tree to DLL.cpp
@@ -1,3 +1,5 @@
+//Method-1 : Time Complexity O(n) , Space Complexity O(n) , Auxillary Space Complexity O(h)
+
 void inorder(Node *root,vector<int>&vec)
 {
     if(root==NULL)
@@ -28,6 +30,42 @@ class Solution
             temp->left=ptr;
             ptr=ptr->right;
         }
+        ptr->right=NULL;
+        Node *temp=dummy_head->right;
+        temp->left=NULL;
+        return temp;
+    }
+};
+
+//Method-2 : Time Complexity O(n) , Space Complexity O(1) , Auxillary Space Complexity O(h)
+
+void inorder(Node *root,vector<int>&vec,Node *&ptr)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    inorder(root->left,vec,ptr);
+    Node *temp=new Node();
+    temp->data=root->data;
+    ptr->right=temp;
+    temp->left=ptr;
+    ptr=ptr->right;
+    inorder(root->right,vec,ptr);
+    
+}
+// This function should return head to the DLL
+class Solution
+{
+    public: 
+    //Function to convert binary tree to doubly linked list and return it.
+    Node * bToDLL(Node *root)
+    {
+        vector<int>vec;
+        
+        Node *dummy_head=new Node();
+        Node *ptr=dummy_head;
+        inorder(root,vec,ptr);
         ptr->right=NULL;
         Node *temp=dummy_head->right;
         temp->left=NULL;
