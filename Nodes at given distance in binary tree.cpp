18fcfb3206928438d33dd5ee911eeb57579a6e1e
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-31 07:28:49 +0530

    Update Nodes at given distance in binary tree.cpp

--- Nodes at given distance in binary tree.cpp
+++ Nodes at given distance in binary tree.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 vector<int>res;
 
 void find_in_child(Node *root,int k)
@@ -66,3 +68,62 @@ public:
         return res;
     }
 };
+
+
+//Method-2 : 
+
+vector<int>res;
+
+void find_in_child(Node *root,int k)
+{
+    if(k<0)
+    {
+        return;
+    }
+    if(root==NULL)
+    {
+        return;
+    }
+    if(k==0)
+    {
+        res.push_back(root->data);
+        return;
+    }
+    find_in_child(root->left,k-1);
+    find_in_child(root->right,k-1);
+}
+int distance(Node *root,int target,int k)
+{
+    if(root==NULL)
+    {
+        return -1;
+    }
+    if(root->data==target)
+    {
+        find_in_child(root,k);
+        return 0;
+    }
+    int l=distance(root->left,target,k);
+    if(l>=0)
+    {
+        find_in_child(root->right,k-l-2);
+        if(k-l-2==-1)
+        {
+            res.push_back(root->data);
+        }
+        return l+1;
+        
+    }
+    int r=distance(root->right,target,k);
+    if(r>=0)
+    {
+        find_in_child(root->left,k-r-2);
+        if(k-r-2==-1)
+        {
+            res.push_back(root->data);
+        }
+        return r+1;
+    }
+    
+    return -1;
+}
