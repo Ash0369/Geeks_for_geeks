3eb8cefb2150d23dfc29db3031015bb1808cad2e
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-01 10:23:16 +0530

    Update Rod Cutting.cpp

--- Rod Cutting.cpp
+++ Rod Cutting.cpp
@@ -92,3 +92,98 @@ class Solution
         return vec[n];
     }
 };
+
+
+//Method-4 : Onlines of Infinite Knapsack problem : 
+
+int cut(int price[],int index,int n,vector<vector<int>>&dp)
+{
+    if(index==0)
+    {
+        return 0;
+    }
+    if(dp[index][n]==-1)
+    {
+        int pick=INT_MIN;
+        if(n>=index)
+        {
+            pick=price[index-1]+cut(price,index,n-index,dp);
+        }
+        int not_pick=cut(price,index-1,n,dp);
+        dp[index][n]=max(pick,not_pick);
+    }
+    return dp[index][n];
+}
+class Solution
+{
+  public:
+    int cutRod(int price[], int n) 
+    {
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return cut(price,n,n,dp);
+    }
+};
+
+//Method-5 : 
+
+class Solution
+{
+  public:
+    int cutRod(int price[], int n) 
+    {
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        
+        for(int j=0;j<=n;j++)
+        {
+            dp[0][j]=0;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                int pick=INT_MIN;
+                if(j>=i)
+                {
+                    pick=price[i-1]+dp[i][j-i];
+                }
+                int not_pick=dp[i-1][j];
+                dp[i][j]=max(pick,not_pick);
+            }
+        }
+        return dp[n][n];
+    }
+};
+
+//Method-6 : 
+
+class Solution
+{
+  public:
+    int cutRod(int price[], int n) 
+    {
+        vector<int>dp(n+1,0);
+        vector<int>prev(n+1,0);
+        for(int j=0;j<=n;j++)
+        {
+            dp[j]=0;
+            prev[j]=0;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                int pick=INT_MIN;
+                if(j>=i)
+                {
+                    pick=price[i-1]+prev[j-i];
+                }
+                int not_pick=dp[j];
+                prev[j]=max(pick,not_pick);
+            }
+            dp=prev;
+        }
+        return dp[n];
+    }
+};
