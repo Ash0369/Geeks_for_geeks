511e0b7d615b40f1e140138f846ae8a5478f2552
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-06 05:13:27 +0530

    Update Longest Common Substring.cpp

--- Longest Common Substring.cpp
+++ Longest Common Substring.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 class Solution 
 {
 public:
@@ -23,6 +25,41 @@ public:
                     dp[i][j]=0;
                 }
              }
+        }
+        return max_value;
+        
+    }
+};
+
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    int longestCommonSubstr(string text1,string text2,int n,int m) 
+    {
+        int n1=text1.size();
+        int n2=text2.size();
+        vector<int>dp(n2+1,0);
+        vector<int>prev(n2+1,0);
+        int max_value=0;
+        
+        for(int i=1;i<=n1;i++)
+        {
+            for(int j=1;j<=n2;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                {
+                       prev[j]=1+dp[j-1];
+                       max_value=max(max_value,prev[j]);
+                }
+                else
+                {
+                    prev[j]=0;
+                }
+             }
+             dp=prev;
         }
         return max_value;
         
