6482b9d4800a1ebf0c97ff6db9b70fb04fe92235
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-05 06:41:13 +0530

    Update Preorder Traversal.cpp

--- Preorder Traversal.cpp
+++ Preorder Traversal.cpp
@@ -1,3 +1,5 @@
+//Method-1 : Normal way
+
 void traversal(Node* root ,vector<int>&res)
 {
     if(root==NULL)
@@ -13,4 +15,42 @@ vector <int> preorder(Node* root)
     vector<int>res;
     traversal(root,res);
     return res;
+}
+
+
+//Method-2 : Morris Traversal 
+
+vector <int> preorder(Node* root)
+{
+    vector<int>preorder;
+    Node *cur=root;
+    while(cur!=NULL)
+    {
+        if(cur->left==NULL)
+        {
+            preorder.push_back(cur->data);
+            cur=cur->right;
+        }
+        else
+        {
+            Node *prev=cur->left;
+            while(prev->right && prev->right!=cur)
+            {
+                prev=prev->right;
+            }
+            if(prev->right==NULL)
+            {
+                preorder.push_back(cur->data);
+                prev->right=cur; //Thread
+                cur=cur->left;
+            }
+            else
+            {
+                prev->right=NULL;
+                cur=cur->right;
+            }
+        }
+        
+    }
+    return preorder;
 }
