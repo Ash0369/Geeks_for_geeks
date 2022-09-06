c5ba0f3e974652a1e01663520bbcaeea1e419fc5
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-06 22:15:06 +0530

    Update Number of occurrence.cpp

--- Number of occurrence.cpp
+++ Number of occurrence.cpp
@@ -16,3 +16,38 @@ public:
 	    return count;
 	}
 };
+
+
+//Method-2 : O(log(n)) Binary search:
+
+int search(int arr[],int l,int r,int k)
+{
+    if(r<l)
+    {
+        return 0;
+    }
+    int mid=(l+r)/2;
+    int ele=arr[mid];
+    if(ele==k)
+    {
+        int a=search(arr,l,mid-1,k);
+        int b=search(arr,mid+1,r,k);
+        return 1+a+b;
+    }
+    if(ele>k)
+    {
+        return  search(arr,l,mid-1,k);
+    }
+    
+    return search(arr,mid+1,r,k);
+    
+}
+
+class Solution
+{
+public:	
+	int count(int arr[], int n, int x) 
+	{
+	    return search(arr,0,n-1,x);
+	}
+};
