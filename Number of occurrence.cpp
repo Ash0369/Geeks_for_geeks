f47baf20ca1ee81caf6f24db58e8686096dc7743
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-06 21:59:58 +0530

    Create Number of occurrence.cpp

---
+++ Number of occurrence.cpp
@@ -0,0 +1,18 @@
+//Method-1 : O(n) Time Complexity
+
+class Solution
+{
+public:	
+	int count(int arr[], int n, int x) 
+	{
+	    int count=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]==x)
+	        {
+	            count++;
+	        }
+	    }
+	    return count;
+	}
+};
