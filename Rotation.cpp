e3c8f480c63e4b15b92f769469f5c77c6ee369d3
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-08 16:03:15 +0530

    Update Rotation.cpp

--- Rotation.cpp
+++ Rotation.cpp
@@ -1,3 +1,5 @@
+//Method-1 :
+
 class Solution
 {
 public:	
@@ -20,4 +22,44 @@ public:
 	    }
 	    return count;
 	}
+};
+
+
+//Method-2 : 
+
+int binary_search(int arr[],int n,int l,int r)
+{
+    if(l>r)
+    {
+        return 0;
+    }
+    int index=(l+r)/2;
+    
+    int next=(index+1)%n;
+    int prev=(index+n-1)%n;
+    
+    
+    if(arr[next]>arr[index] && arr[prev]>arr[index])
+    {
+        return index;
+    }
+    //go in direction where array is not sorted
+    
+    if(arr[0]<=arr[index])
+    {
+        return binary_search(arr,n,index+1,r);
+    }
+    else if(arr[index]<=arr[n-1])
+    {
+        return binary_search(arr,n,l,index-1);
+    }
+}
+class Solution
+{
+public:	
+	int findKRotation(int arr[], int n) 
+	{
+	    return binary_search(arr,n,0,n-1);
+	}
+	
 };
