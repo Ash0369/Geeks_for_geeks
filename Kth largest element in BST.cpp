996df011a9df9383cad4d41cbdf660a191789dee
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-10 12:35:39 +0530

    Update Kth largest element in BST.cpp

--- Kth largest element in BST.cpp
+++ Kth largest element in BST.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 int counter=0;
 int result;
 void traversal(Node* root,int k)
@@ -26,3 +28,35 @@ class Solution
         return result;
     }
 };
+
+//Method-2 : 
+
+int large(Node *root,int &k)
+{
+    if(root==NULL)
+    {
+        return -1;
+    }
+    int a=large(root->right,k);
+    if(a!=-1)
+    {
+        return a;
+    }
+    k--;
+    if(k==0)
+    {
+        return root->data;
+    }
+    return large(root->left,k);
+}
+
+class Solution 
+{
+  public:
+    
+    int kthLargest(Node *root, int k)
+    {
+        return large(root,k);
+    }
+};
+
