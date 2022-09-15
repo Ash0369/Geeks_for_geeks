78316709b894fd8fb44c35380d1af2b22ba7fed2
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-15 12:57:16 +0530

    Update Search in a row-column sorted Matrix.cpp

--- Search in a row-column sorted Matrix.cpp
+++ Search in a row-column sorted Matrix.cpp
@@ -19,3 +19,33 @@ class Solution
         return false;
     }
 };
+
+
+//Method-2 : 
+
+int bs(vector<vector<int> >&matrix,int n,int m,int key)
+{
+    if(n>=matrix.size() || m<0)
+    {
+        return false;
+    }
+    int val=matrix[n][m];
+    if(val==key)
+    {
+        return true;
+    }
+    else if(val>key)
+    {
+        return bs(matrix,n,m-1,key);
+    }
+    return bs(matrix,n+1,m,key);
+}
+class Solution
+{
+    public:
+    //Function to search a given number in row-column sorted matrix.
+    bool search(vector<vector<int> > matrix, int n, int m, int key) 
+    {
+        return bs(matrix,0,m-1,key);
+    }
+};
