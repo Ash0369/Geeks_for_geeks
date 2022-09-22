14ddc5ea628451d5838464abac5449138a680511
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-22 17:14:08 +0530

    Update Number of Coins.cpp

--- Number of Coins.cpp
+++ Number of Coins.cpp
@@ -2,7 +2,7 @@ int coins(int vec[],int target,int index,int m,vector<vector<int>>&dp)
 {
     if(index>=m || target<0)
     {
-        return -2;
+        return -1;
     }
     if(target==0)
     {
@@ -12,11 +12,11 @@ int coins(int vec[],int target,int index,int m,vector<vector<int>>&dp)
     {
         int a=1+coins(vec,target-vec[index],index,m,dp);
         int b=coins(vec,target,index+1,m,dp);
-        if(a<0)
+        if(a<=0)
         {
             dp[index][target]=b;
         }
-        else if(b<0)
+        else if(b<=0)
         {
             dp[index][target]=a;
         }
@@ -25,19 +25,26 @@ int coins(int vec[],int target,int index,int m,vector<vector<int>>&dp)
              dp[index][target]=min(a,b);
         }
     }
+    
     return dp[index][target];
 }
+
 class Solution
 {
+
+
 	public:
 	int minCoins(int coin[], int M, int V) 
 	{ 
 	    vector<vector<int>>dp(M+1,vector<int>(V+1,-1));
 	    int ans=coins(coin,V,0,M,dp);
-	    if(ans<0)
+	    if(ans<=0)
 	    {
 	        return -1;
 	    }
+	    
 	    return ans;
+	    
 	} 
+	  
 };
