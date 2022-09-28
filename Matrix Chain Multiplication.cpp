25e8e28929df40ad8ec37d719166e97785ed4466
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-28 07:18:10 +0530

    Update Matrix Chain Multiplication.cpp

--- Matrix Chain Multiplication.cpp
+++ Matrix Chain Multiplication.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 class Solution
 {
 public:
@@ -23,5 +25,40 @@ public:
     {
         vector<vector<int>>dp(N,vector<int>(N,-1));
         return multiply(arr,1,N-1,dp);
+    }
+};
+
+
+//Method-2 :
+
+class Solution
+{
+public:
+    int matrixMultiplication(int N, int arr[])
+    {
+        vector<vector<int>>dp(N,vector<int>(N,0));
+        for(int i=0;i<N;i++)
+        {
+            dp[i][i]=0;
+        }
+        
+        for(int i=N-1;i>0;i--)
+        {
+            for(int j=i+1;j<N;j++) //As j will be always right of i
+            {
+                int res=INT_MAX;
+                if(dp[i][j]==0)
+                {
+                    for(int k=i;k<j;k++)
+                    {
+                        int temp=(arr[i-1]*arr[k]*arr[j])+dp[i][k]+dp[k+1][j];
+                        res=min(res,temp);
+                    }
+                    dp[i][j]=res;
+                }
+              
+            }
+        }
+        return dp[1][N-1];
     }
 };
