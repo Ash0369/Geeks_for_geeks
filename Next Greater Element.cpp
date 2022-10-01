ff9bdd747ed8bdc754a60ca0dad7cb496a234421
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-01 11:23:55 +0530

    Create Next Greater Element.cpp

---
+++ Next Greater Element.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+    #define ll long long 
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n)
+    {
+        stack<ll int>st;
+        vector<ll int>ans;
+        for(int i=n-1;i>=0;i--)
+        {
+            
+            while(!st.empty() && st.top()<=arr[i])
+            {
+                st.pop();
+            }
+            if(st.empty())
+            {
+                ans.push_back(-1);
+            }
+            else
+            {
+                ans.push_back(st.top());
+            }
+            st.push(arr[i]);
+            
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
