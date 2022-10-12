6899e39df19ad6fa6a0bb58f3a9f18915cab1a27
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-12 07:07:36 +0530

    Update BFS of graph.cpp

--- BFS of graph.cpp
+++ BFS of graph.cpp
@@ -25,3 +25,43 @@ class Solution {
         return bfs;
     }
 };
+
+
+//If graph has more then 1 components that are not connected then we have to use differennt way : 
+
+
+class Solution 
+{
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int v, vector<int> adj[]) 
+    {
+        vector<int>visited(v+2);
+        vector<int>bfs;
+        
+        for(int i=0;i<v;i++)
+        {
+            if(!visited[i])
+            {
+                queue<int>q;
+                q.push(i);
+                while(!q.empty())
+                {
+                    int node=q.front();
+                    q.pop();
+                    bfs.push_back(node);
+                    for(auto x:adj[node])
+                    {
+                        if(!visited[x])
+                        {
+                            q.push(x);
+                            visited[x]=1;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return bfs;
+    }
+};
