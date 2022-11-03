8a582ce5b0aba0b281f44d218ff9c889c231cfb0
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-03 06:59:40 +0530

    Update Implementing Dijkstra Algorithm.cpp

--- Implementing Dijkstra Algorithm.cpp
+++ Implementing Dijkstra Algorithm.cpp
@@ -1,3 +1,5 @@
+//Method-1 : Priority queue
+
 class Solution
 {
 	public:
@@ -21,5 +23,37 @@ class Solution
             }
         }
         return distance;
+    }
+};
+
+
+//Method-2 : Using Set
+
+class Solution
+{
+	public:
+    vector <int> dijkstra(int v, vector<vector<int>>adj[], int s)
+    {
+        vector<int>distance(v,INT_MAX);
+        distance[s]=0;
+        set<pair<int,int>>st;
+        st.insert(make_pair(0,s));
+        while(!st.empty())
+        {
+            auto node=st.begin();
+            st.erase(node);
+            int front=(*node).second;
+            for(auto ele:adj[front]) //ele will contain all vectors which are adjcent to nodes
+            {
+                if(distance[ele[0]]>(distance[front]+ele[1]))
+                {
+                    st.erase({distance[ele[0]],ele[0]});
+                    distance[ele[0]]=distance[front]+ele[1];
+                    st.insert(make_pair(distance[ele[0]],ele[0]));
+                }
+            }
+        }
+        return distance;
+
     }
 };
