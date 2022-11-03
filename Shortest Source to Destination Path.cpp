d488dbe38128f4a07a49021b5f84b19c30b59a2b
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-03 15:47:45 +0530

    Update Shortest Source to Destination Path.cpp

--- Shortest Source to Destination Path.cpp
+++ Shortest Source to Destination Path.cpp
@@ -1,3 +1,6 @@
+//Method-1 : Using Priority queue : 
+
+
 class Solution 
 {
   public:
@@ -56,3 +59,62 @@ class Solution
         return distance[x][y];
     }
 };
+
+
+//Method-2 : Optimized :
+
+
+class Solution 
+{
+  public:
+    int shortestDistance(int n, int m, vector<vector<int>>vec, int x, int y) 
+    {
+        if(vec[0][0]==0)
+        {
+            return -1;
+        }
+        queue<pair<int,int>>q;
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        q.push({0,0});
+        vis[0][0]=true;
+        int dx[4]={-1,0,1,0};
+        int dy[4]={0,1,0,-1};
+        int dit=0;
+        bool res=false;
+        while(!q.empty() && !res)
+        {
+            int n1=q.size();
+            for(int i=0;i<n1;i++)
+            {
+                auto node=q.front();
+                q.pop();
+                
+                int x1=node.first;
+                int y1=node.second;
+                
+                
+                for(int j=0;j<4;j++)
+                {
+                    int nx=x1+dx[j];
+                    int ny=y1+dy[j];
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && vec[nx][ny]==1 && vis[nx][ny]==false)
+                    {
+                        vis[nx][ny]=true;
+                        q.push({nx,ny});
+                        if(nx==x && ny==y)
+                        {
+                            res=true;
+                            n1=0;
+                            break;
+                        }
+                    }
+                }
+            }
+            dit++;
+        }
+        if(res==false)
+        {
+            return -1;
+        }
+        return dit;
+    }
