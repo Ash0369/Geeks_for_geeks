e46c6f69bf2c80b5b960d865905d243e58109689
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-05 10:41:08 +0530

    Update Bellman-Ford Algorithm.cpp

--- Bellman-Ford Algorithm.cpp
+++ Bellman-Ford Algorithm.cpp
@@ -1,21 +1,38 @@
-class Solution
+class Solution 
 {
-	public:
-    vector <int> bellman_ford(int v, vector<vector<int>> adj, int s) 
+  public:
+    /*  Function to implement Bellman Ford
+    *   edges: vector of vectors which represents the graph
+    *   S: source vertex to start traversing graph with
+    *   V: number of vertices
+    */
+    vector<int> bellman_ford(int v, vector<vector<int>>& edges, int S) 
     {
         vector<int>distance(v,1e8);
-        distance[s]=0;
-        int n=adj.size();
-        //we have to v-1 relaxations.
+        distance[S]=0;
         for(int i=0;i<v;i++)
         {
-            for(auto x:adj)
+            for(auto x:edges)
             {
-                if(distance[x[1]]>(distance[x[0]]+x[2]))
+                if(distance[x[0]]+x[2]<distance[x[1]])
                 {
                     distance[x[1]]=distance[x[0]]+x[2];
                 }
             }
+        }
+        bool cycle=false;
+        //If nth iteration also reduction so -ve cycle
+        for(auto x:edges)
+        {
+            if(distance[x[0]]+x[2]<distance[x[1]])
+            {
+                cycle=true;
+                distance[x[1]]=distance[x[0]]+x[2];
+            }
+        }
+        if(cycle)
+        {
+            return {-1};
         }
         return distance;
     }
