c9a35acd19a956a776aaed7132c6235ad54faa13
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-06 09:40:14 +0530

    Update Minimum Spanning Tree.cpp

--- Minimum Spanning Tree.cpp
+++ Minimum Spanning Tree.cpp
@@ -98,3 +98,239 @@ class Solution
          return sum;
     }
 };
+
+
+//Method-3 : Using Union by size
+
+class dsu
+{
+    vector<int>rank,parent,size;
+    public:
+    dsu(int n)
+    {
+        rank.resize(n+1,0);//Work for both 0 or 1 based
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+
+    int find_parent(int node)
+    {
+        if(node==parent[node])
+        {
+            return node;
+        }
+        return parent[node]=find_parent(parent[node]);//Path compression
+    }
+
+    void union_by_rank(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(rank[ultimate_parent_u]<rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+        }
+        else if(rank[ultimate_parent_u]>rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+        else if(rank[ultimate_parent_u]==rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            rank[ultimate_parent_u]++;
+        }
+    }
+
+    void union_by_size(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(size[ultimate_parent_u]<size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+            size[ultimate_parent_v]+=size[ultimate_parent_u];
+        }
+        else if(size[ultimate_parent_u]>size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+        else if(size[ultimate_parent_u]==size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+    }
+
+};
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int v, vector<vector<int>> adj[])
+    {
+        vector<pair<int,pair<int,int>>>vec;
+        for(int i=0;i<v;i++)
+        {
+            for(auto it:adj[i])
+            {
+                int node=it[0];
+                int wt=it[1];
+                
+                vec.push_back({wt,{i,node}});
+            }
+        }
+        
+        sort(vec.begin(),vec.end());
+        int mstwt=0;
+        dsu ds(v);
+        for(auto it:vec)
+        {
+            int u=it.second.first;
+            int v=it.second.second;
+            int wt=it.first;
+            
+            if(ds.find_parent(u)!=ds.find_parent(v))
+            {
+                mstwt+=wt;
+                ds.union_by_size(u,v);
+            }
+        }
+        return mstwt;
+    }
+};
+
+//Method-4 : Using Union by rank
+
+
+class dsu
+{
+    vector<int>rank,parent,size;
+    public:
+    dsu(int n)
+    {
+        rank.resize(n+1,0);//Work for both 0 or 1 based
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+
+    int find_parent(int node)
+    {
+        if(node==parent[node])
+        {
+            return node;
+        }
+        return parent[node]=find_parent(parent[node]);//Path compression
+    }
+
+    void union_by_rank(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(rank[ultimate_parent_u]<rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+        }
+        else if(rank[ultimate_parent_u]>rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+        else if(rank[ultimate_parent_u]==rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            rank[ultimate_parent_u]++;
+        }
+    }
+
+    void union_by_size(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(size[ultimate_parent_u]<size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+            size[ultimate_parent_v]+=size[ultimate_parent_u];
+        }
+        else if(size[ultimate_parent_u]>size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+        else if(size[ultimate_parent_u]==size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+    }
+
+};
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int v, vector<vector<int>> adj[])
+    {
+        vector<pair<int,pair<int,int>>>vec;
+        for(int i=0;i<v;i++)
+        {
+            for(auto it:adj[i])
+            {
+                int node=it[0];
+                int wt=it[1];
+                
+                vec.push_back({wt,{i,node}});
+            }
+        }
+        
+        sort(vec.begin(),vec.end());
+        int mstwt=0;
+        dsu ds(v);
+        for(auto it:vec)
+        {
+            int u=it.second.first;
+            int v=it.second.second;
+            int wt=it.first;
+            
+            if(ds.find_parent(u)!=ds.find_parent(v))
+            {
+                mstwt+=wt;
+                ds.union_by_rank(u,v);
+            }
+        }
+        return mstwt;
+    }
+};
