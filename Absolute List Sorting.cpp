cfb42e8780b8f367e2ea68e8dfd0e4b76b76bdb3
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-12-21 06:35:39 +0530

    Create Absolute List Sorting.cpp

---
+++ Absolute List Sorting.cpp
@@ -0,0 +1,55 @@
+Node* reverse(Node *head)
+{
+    if(head==NULL || head->next==NULL)
+    {
+        return head;
+    }
+    Node *g=reverse(head->next);
+    head->next->next=head;
+    head->next=NULL;
+    return g;
+}
+class Solution
+{
+    
+public:
+    Node* sortList(Node* head)
+    {
+        Node *neg=new Node;
+        Node *pos=new Node;
+        
+        Node *poshead=pos;
+        Node *neghead=neg;
+        
+        while(head!=NULL)
+        {
+            if((head->data)>=0)
+            {
+                pos->next=head;
+                head=head->next;
+                pos=pos->next;
+                pos->next=NULL;
+            }
+            else
+            {
+                neg->next=head;
+                head=head->next;
+                neg=neg->next;
+                neg->next=NULL;
+            }
+        }
+        
+        neghead=reverse(neghead->next);
+        neg=neghead;
+        while(neg!=NULL && neg->next!=NULL)
+        {
+            neg=neg->next;
+        }
+        if(neg==NULL)
+        {
+            return poshead->next;
+        }
+        neg->next=poshead->next;
+        return neghead;
+    }
+};
