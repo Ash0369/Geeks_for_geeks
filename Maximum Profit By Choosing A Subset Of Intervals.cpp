aaf49eaddc000861b4cf4332146c123d29d5ad05
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-01-04 12:03:51 +0530

    Update Maximum Profit By Choosing A Subset Of Intervals.cpp

--- Maximum Profit By Choosing A Subset Of Intervals.cpp
+++ Maximum Profit By Choosing A Subset Of Intervals.cpp
@@ -43,3 +43,43 @@ class Solution
         return profit(0,intervals,-1,dp);
     }
 };
+
+
+//Method-2 : 
+
+bool cmp(vector<int>&vec1,vector<int>&vec2)
+{
+    if(vec1[0]<vec2[0])
+    {
+        return true;
+    }
+    return false;
+}
+class Solution 
+{
+  public:
+    int maximum_profit(int n, vector<vector<int>> &intervals) 
+    {
+        sort(intervals.begin(),intervals.end(),cmp);
+        
+        //dp[i][j] --> Max profit we can get if we start chossing from ith interval
+        //and last interval we picked is j-1.
+        
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=-1;j<=i;j++)
+            {
+                int not_pick=dp[i+1][j+1];
+                int pick=0;
+                if(j==-1 || intervals[j][1]<=intervals[i][0])
+                {
+                    pick=intervals[i][2]+dp[i+1][i+1];
+                }
+                dp[i][j+1]=max(pick,not_pick);
+            }
+        }
+        
+        return dp[0][0];
+    }
+};
