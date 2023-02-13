ef911188b6bcc5c882277b198ddb2f655f97c82a
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-02-13 06:44:08 +0530

    Create Arithmetic Number.cpp

---
+++ Arithmetic Number.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int inSequence(int A, int B, int C)
+    {
+        if(C==0)
+        {
+            return A==B;
+        }
+
+        if((B>=A && C>=0) || (B<=A && C<=0))
+        {
+            return (B-A)%C==0;
+        }
+        return 0;
+        
+        
+    }
+};
