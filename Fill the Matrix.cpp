34c0c8be3d3d6f9eaf9770fe5edfee2a3a0d4c94
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-02-21 06:40:37 +0530

    Create Fill the Matrix.cpp

---
+++ Fill the Matrix.cpp
@@ -0,0 +1,46 @@
+class Solution
+{   
+public:
+    int minIteration(int N, int M, int x, int y)
+    {
+        int n=N;
+        int m=M;
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        queue<pair<int,int>>q;
+        q.push({x-1,y-1});
+        dp[x-1][y-1]=1;
+        
+        int dx[4]={-1,0,1,0};
+        int dy[4]={0,1,0,-1};
+        int cost=0;
+        
+        while(!q.empty())
+        {
+            int sz=q.size();
+            bool res=false;
+            for(int j=0;j<sz;j++)
+            {
+                int x=q.front().first;
+                int y=q.front().second;
+                q.pop();
+                for(int i=0;i<4;i++)
+                {
+                    int nx=x+dx[i];
+                    int ny=y+dy[i];
+                    
+                    if(nx>=0 && ny>=0 && nx<n && ny<m && dp[nx][ny]==0)
+                    {
+                        res=true;
+                        dp[nx][ny]=1;
+                        q.push({nx,ny});
+                    }
+                }
+            }
+            if(res==true)
+            {
+                cost++;
+            }
+        }
+        return cost;
+    }
+};
