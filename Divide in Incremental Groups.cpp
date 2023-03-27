3badf55ff05a30fbeec49eb324e0f9ba9bbe1648
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-03-27 05:49:14 +0530

    Update Divide in Incremental Groups.cpp

--- Divide in Incremental Groups.cpp
+++ Divide in Incremental Groups.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 int dp[105][105][105];
 int ways(int n,int k,int last)
 {
@@ -28,5 +30,41 @@ class Solution
     {
         memset(dp,-1,sizeof(dp));
         return ways(N,K,1);
+    }
+};
+
+//Method-2 : 
+
+int dp[105][105];
+class Solution 
+{
+  public:
+    int countWaystoDivide(int N, int K) 
+    {
+        int n=N;
+        int k=K;
+        memset(dp,0,sizeof(dp));
+        if(n<k)
+        {
+            return 0;
+        }
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            dp[i][1]=1;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=2;j<=k;j++)
+            {
+                if(i<j)
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                    continue;
+                }
+               dp[i][j]=dp[i-j][j]+dp[i-1][j-1];
+            }
+        }
+        return dp[n][k];
     }
 };
