58df21d768fbef6573dfe1238e9ebbd8cedae3ab
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-03-29 06:25:36 +0530

    Update Count the Substrings.cpp

--- Count the Substrings.cpp
+++ Count the Substrings.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 class Solution
 {
     public:
@@ -26,6 +28,37 @@ class Solution
                 }
             }
         }
+        return ans;
+    }
+};
+
+//Method2 : 
+
+class Solution
+{
+    public:
+    int countSubstring(string S)
+    {
+        int ans=0;
+        string s=S;
+        int n=s.size();
+        map<int,int>mp;
+        mp[0]=1;
+        int tot=0;
+        for(int j=0;j<n;j++)
+        {
+            if(int(s[j])>=97)
+            {
+                tot++;
+            }
+            else
+            {
+                tot--;
+            }
+            ans+=mp[tot];
+            mp[tot]++;
+        }
+       
         return ans;
     }
 };
