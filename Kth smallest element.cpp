8bd2d7c1c5e18ff21cf959a7940a08486a58df01
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-04-19 16:25:52 +0530

    Update Kth smallest element.cpp

--- Kth smallest element.cpp
+++ Kth smallest element.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 class Solution
 {
     public:
@@ -6,5 +8,47 @@ class Solution
         int n=r-l+1;
         sort(arr,arr+n);
         return arr[k-1];
+    }
+};
+
+//Method-2 : Quick select
+
+
+int indx;
+int quick(int arr[],int l,int r)
+{
+   
+    int grt=l;
+    int pivot=arr[r];
+    for(int i=l;i<r;i++)
+    {
+        if(arr[i]>pivot)
+        {
+            continue;
+        }
+        swap(arr[i],arr[grt]);
+        grt++;
+    }
+    swap(arr[r],arr[grt]);
+    if(indx==grt)
+    {
+        return arr[indx];
+    }
+    else if(indx>grt)
+    {
+        return quick(arr,grt+1,r);
+    }
+    return quick(arr,l,grt-1);
+}
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) 
+    {
+        indx=k-1;
+        return quick(arr,l,r);
     }
 };
