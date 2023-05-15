70bfc772ee9083a4a25cbf1f410a8e5c187ca908
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-05-15 19:12:53 +0530

    Update Coin Change.cpp

--- Coin Change.cpp
+++ Coin Change.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 #define ll long long
 ll int ways(int index,int coins[],int n,int sum,vector<vector<ll int>>&dp)
 {
@@ -26,5 +28,32 @@ class Solution
         return ways(0,coins,n,sum,dp);
         
         
+    }
+};
+
+
+//Method-2 : 
+
+class Solution 
+{
+  public:
+    long long int count(int coins[], int N, int sum) 
+    {
+        
+        int n=N;
+        vector<vector<ll int>>dp(n+5,vector<ll int>(sum+5,0));
+        dp[n][0]=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=sum;j++)
+            {
+                dp[i][j]=dp[i+1][j];
+                if(j-coins[i]>=0)
+                {
+                    dp[i][j]+=dp[i][j-coins[i]];
+                }
+            }
+        }
+        return dp[0][sum];
     }
 };
