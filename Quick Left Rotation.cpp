90db192de683f632b7a0abc55152b8fc6fd710bf
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-05-17 07:07:54 +0530

    Create Quick Left Rotation.cpp

---
+++ Quick Left Rotation.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+	public:
+	void leftRotate(int arr[], int k, int n) 
+	{ 
+	    k=k%n;
+	    reverse(arr,arr+k);
+	    reverse(arr+k,arr+n);
+	    reverse(arr,arr+n);
+	    
+	} 
+};
