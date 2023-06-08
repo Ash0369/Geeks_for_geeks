614a5fe42f4d69ec00552bb123b090f3482ab141
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-06-08 10:49:28 +0530

    Create Find Kth permutation.cpp

---
+++ Find Kth permutation.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    string kthPermutation(int n, int k)
+    {
+        string ans="";
+        for(int i=1;i<=n;i++)
+        {
+            ans.push_back(i+'0');
+        }
+        for(int i=1;i<k;i++)
+        {
+            next_permutation(ans.begin(),ans.end());
+        }
+        return ans;
+    }
+};
