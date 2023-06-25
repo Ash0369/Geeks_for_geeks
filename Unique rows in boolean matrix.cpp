36410915a0b26105a45bd4a49642355fbff87386
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-06-25 06:24:08 +0530

    Create Unique rows in boolean matrix.cpp

---
+++ Unique rows in boolean matrix.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+    // #define MAX 1000
+    vector<vector<int>> uniqueRow(int M[MAX][MAX],int row,int col)
+    {
+        set<vector<int>>st;
+        vector<vector<int>>ans;
+        for(int i=0;i<row;i++)
+        {
+            vector<int>v;
+            for(int j=0;j<col;j++)
+            {
+                v.push_back(M[i][j]);
+            }
+            if(st.find(v)!=st.end())
+            {
+                continue;
+            }
+            else
+            {
+                st.insert(v);
+                ans.push_back(v);
+            }
+            
+        }
+        return ans;
+    }
+};
