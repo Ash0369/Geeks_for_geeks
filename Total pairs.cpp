0c03bf835f9262191eec6c53f43f703af8bdeab1
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-07-30 19:57:49 +0530

    Create Total pairs.cpp

---
+++ Total pairs.cpp
@@ -0,0 +1,53 @@
+class Solution 
+{
+public:
+	int TotalPairs(vector<int>&nums, int x, int y)
+	{
+	    //3 5 7  7 7 7 9 9
+	    //1 2 4 4  10 11 12 12 12 13 15 
+	    int cnt=0;
+	    sort(nums.begin(),nums.end());
+	    int n=nums.size();
+	    for(int i=0;i<n;i++)
+	    {
+	        int ele=nums[i];
+	        
+	        //we should get x
+	        
+	        int lower=(x+ele-1)/ele;
+	        
+	        int upper=(y+ele-1)/ele;
+	        if((upper*ele)<=y)
+	        {
+	            upper++;
+	        }
+	        //lower=max(lower,ele);
+	        //upper=max(upper,ele);
+	        
+	        auto index=lower_bound(nums.begin(),nums.end(),lower);
+	        auto index1=lower_bound(nums.begin(),nums.end(),upper);
+	        if(index==nums.end() && index1==nums.end())
+	        {
+	            continue;
+	        }
+	        int ans=(index-nums.begin());
+	        ans=max(i+1,ans);
+	        if(index1!=nums.end())
+	        {
+	            int temp=index1-nums.begin();
+	            temp=max(temp,i+1);
+	            temp--;
+	            
+	            ans=(temp-(ans-1));
+	        }
+	        else
+	        {
+	            ans=(n-1-(ans-1));
+	        }
+	        cnt+=ans;
+	        
+	        
+	    }
+	    return cnt;
+	}
+};
