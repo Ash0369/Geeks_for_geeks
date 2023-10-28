4167eaa890defc3b320caec19c9ac2544839d9d1
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-10-28 06:19:27 +0530

    Create Bleak Numbers.cpp

---
+++ Bleak Numbers.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+	int is_bleak(int n)
+	{
+	    for(int i=n-1;i>=max(0,n-30);i--)
+	    {
+	        int b=__builtin_popcount(i);
+	        if(i+b==n)
+	        {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+};
