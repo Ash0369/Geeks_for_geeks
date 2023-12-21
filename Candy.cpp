c3d084f15e4304cf87291959d0ee964fe5bea0f5
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-12-21 09:45:30 +0530

    Create Candy.cpp

---
+++ Candy.cpp
@@ -0,0 +1,25 @@
+#define ll long long
+class Solution 
+{
+  public:
+    int minCandy(int n, vector<int> &ratings) 
+    {
+        
+        vector<int>dp(n,1);
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                dp[i]=dp[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+            {
+                dp[i]=max(dp[i],dp[i+1]+1);
+            }
+        }
+        return accumulate(dp.begin(),dp.end(),0);
+    }
+};
