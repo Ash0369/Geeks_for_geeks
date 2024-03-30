38ac904be5a86296ca0597e4820d68d969355e80
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2024-03-30 08:17:15 +0530

    Create Minimum element in BST.cpp

---
+++ Minimum element in BST.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:   
+    int minValue(Node* root) {
+        if(root->left)
+        {
+            return minValue(root->left);
+        }
+        return root->data;
+    }
+};
